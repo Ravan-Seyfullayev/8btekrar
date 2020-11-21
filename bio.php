15ed4c1428fd9aae62c4271b9bac6d02aca95308
Ravan-Seyfullayev <68846252+Ravan-Seyfullayev@users.noreply.github.com>
2020-11-21 12:11:34 +0400

    8btekrar
    8btekrar

---
+++ bio.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tekrar</title>
+</head>
+<body>
+    <div class="video">
+        <div class="firstChapter">
+            <h1>Birinci bolme</h1>
+    <iframe src="https://www.kaspidistant.com/noduploads/modul/7a67f716fdac15b8b9195d72f7c46a77.mp4" frameborder="0" width="782px" height="439px" allow="fullscreen"></iframe>
+     <iframe src="https://www.kaspidistant.com/noduploads/modul/d4abd02e1ed64e8b793111a2b3f90c07.mp4" frameborder="0" width="782px" height="439px" allow="fullscreen"></iframe>
+    <iframe src="https://www.kaspidistant.com/noduploads/modul/7d50f2cabc623aa2fe7ed687d06ff369.mp4" frameborder="0" width="782px" height="439px" allow="fullscreen"></iframe>
+     <iframe src="https://www.kaspidistant.com/noduploads/modul/8e24f1a417a37e74ff76dce562a1d489.mp4" frameborder="0" width="782px" height="439px" allow="fullscreen"></iframe>
+    <iframe src="https://www.kaspidistant.com/noduploads/modul/2b863828216a7bbc1ef5f1bf74d02454.mp4" frameborder="0" width="782px" height="439px" allow="fullscreen"></iframe>
+    <iframe src="https://www.kaspidistant.com/noduploads/modul/0207ae3f53e4ac9cc9d80eab27fd8229.mp4" frameborder="0" width="782px" height="439px" allow="fullscreen"></iframe>
+        </div>
+        <h1>Ikinci Bolme</h1>
+        <div class="secondChapter">
+        <iframe src="https://www.kaspidistant.com/noduploads/modul/485d074baf973603af10b39a7b087c70.mp4" frameborder="0" width="782px" height="439px" allow="fullscreen"></iframe>
+            <iframe src="https://www.kaspidistant.com/noduploads/modul/09447fea01fe95fcf62db8520ec677dc.mp4" frameborder="0" width="782px" height="439px" allow="fullscreen"></iframe>
+            <iframe src="https://www.kaspidistant.com/noduploads/modul/17df66b1761f990dbdb500aada714075.mp4" frameborder="0" width="782px" height="439px" allow="fullscreen"></iframe>
+        </div>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tekrar</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+<div class="buttons">
+    <button><a href="bio.php">Biologiya</a></button>
+    <button><a href="aztrx.php">Azərbaycan Tarixi</a></button>
+    <button><a href="azdili.php">Azərbaycan dili</a></button>
+    </div>
+</body>
+</html>
